aca02f663400988dbd88ade361a4ce2dc4a275ad
himanshu <himanshumehta1114@gmail.com>
2017-10-11 16:43:54 +0530

    Adding sample data

--- models/todoModel.js
+++ models/todoModel.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var schema = mongoose.Schema;
 
-var todoSchema = new Schema({
+var todoSchema = new schema({
     username : String,
     todo : String,
     isDone : Boolean,
